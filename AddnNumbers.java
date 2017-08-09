fd05cb6c3ae7e349429524a75992ada5dcf28306
Divyayelala <divyayelala2404@gmail.com>
2017-08-09 16:12:45 +0530

    addition of n numbers

---
+++ AddnNumbers.java
@@ -0,0 +1,17 @@
+
+public class AddnNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum=0;
+		for(int i=0;i<=10;i++)
+		{
+			System.out.print( i + " ");
+			sum  = sum+i;
+		}
+		System.out.println();
+		System.out.println("sum of all numbers : "+sum);
+
+	}
+
+}
