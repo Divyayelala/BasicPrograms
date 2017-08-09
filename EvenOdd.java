e9f0ffc1acb2897009cbc224d99b69e1633417c9
Divyayelala <divyayelala2404@gmail.com>
2017-08-09 16:13:01 +0530

    even or odd check

---
+++ EvenOdd.java
@@ -0,0 +1,18 @@
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 22;
+		if(n%2==0)
+		{
+			System.out.println(n + " is even number");
+		}
+		else
+		{
+		System.out.println(n + " is odd number");
+		}
+
+	}
+
+}
