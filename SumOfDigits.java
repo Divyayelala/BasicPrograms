53a1f1278b8041ed6c0dec93ddcc0b5fe4b5cf2c
Divyayelala <divyayelala2404@gmail.com>
2017-08-09 15:42:41 +0530

    sum of digits in a number

---
+++ SumOfDigits.java
@@ -0,0 +1,14 @@
+
+public class SumOfDigits {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a = 24 ;
+		int remainder = a%10;
+		int coefficient = a/10;
+		int sum = remainder + coefficient ;
+		System.out.println(sum);
+	}
+
+}
