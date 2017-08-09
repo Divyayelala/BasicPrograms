3350109550682de1edd3423ec2670e3845c68254
Divyayelala <divyayelala2404@gmail.com>
2017-08-09 15:38:33 +0530

    Factorial of a number

---
+++ Factorial.java
@@ -0,0 +1,15 @@
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 5;
+		int fact = 1;
+		for(int i=1;i<=n;i++)
+		{
+			fact = fact *i;
+		}
+		System.out.println(fact);
+	}
+
+}
