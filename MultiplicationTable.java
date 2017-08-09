69a954f8d9b19b7b94a83957cfcced25ea38dcb7
Divyayelala <divyayelala2404@gmail.com>
2017-08-09 16:13:25 +0530

    multiplication table of given number

---
+++ MultiplicationTable.java
@@ -0,0 +1,14 @@
+
+public class MultiplicationTable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 2;
+		for(int i=1;i<=10;i++)
+		{
+			System.out.println(n + "*" + i + " = " + (n*i));
+		}
+
+	}
+
+}
